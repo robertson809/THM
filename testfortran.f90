2023a000c8d869c4393ec4b0d3783b31dd1f74ee
Michael Robertson <mirobertson@davidson.edu>
2019-02-11 02:01:29 -0500

    Hello World
    Began getting aquainted with fortran: ifs, dos, and saying hello world

---
+++ testfortran.f90
@@ -0,0 +1,26 @@
+program addNumbers
+
+! This simple program adds two numbers
+   implicit none
+
+! Type declarations
+   real :: a, b, result
+   character(len = 80) :: message
+   integer :: test
+
+! Executable statements
+   a = 12.0
+   b = 15.0
+   result = a + b + 1
+   message = "A kindred soul discovered in black sand and steel"
+   print *, message
+   
+   do test = 1, 10
+   		print *, "Iteration"
+		if (test == 5) then
+			exit
+		end if
+		print *, test
+   end do
+   print *, "I made it to line 25"
+end program addNumbers
