3b93a8e11813df35ef75da5d8b8c74f88404f2ec
Michael Robertson <mirobertson@davidson.edu>
2019-02-14 22:47:48 -0500

    Comments

--- tridiag.f90
+++ tridiag.f90
@@ -5,6 +5,11 @@
 ! A bit tautologic here, define dp as the kind that a double is
 ! this just takes whatever this system calls a double and then
 ! makes dp that
+
+! 1.d0 is a intrinsic fortran double
+! 1.e0 is an intrinsic fortran single
+! parameter tag makes it a final, it doesn't change throughout
+! the program
 integer, parameter  :: dp = kind(1.d0)
 module tridiag
 	implicit none
