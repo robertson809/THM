db094404199e87f9a244b2c947d2cc3eb17f9b0f
Michael Robertson <mirobertson@davidson.edu>
2019-02-23 18:06:51 -0500

    Implemented scalar multiplication and matrix addition functions
    Next step is to implement Horner's method

--- tridiag.f90
+++ tridiag.f90
@@ -5,7 +5,7 @@ module tridiag
     !****************************************
     !				type trid				*
     !****************************************
-    !This is a data type that we've defined
+    !This is a data type that we've defined, with the tre
     type trid
         complex(kind=dp), allocatable :: diag(:), upper(:), lower(:)
     end type trid
@@ -14,17 +14,19 @@ contains
     !****************************************
     !				print trid				*
     !****************************************
-    subroutine print_trid(a,n)
+    subroutine print_trid(a)
         implicit none
         ! argument variables
-        integer         :: n
         type(trid)      :: a
         ! local variables
         integer         :: i
+        integer         :: n
+        
+        n = size(a%diag)
 			
         ! print statements
         write(*,'(F0.0,SP,F0.0,"i", F0.0,SP,F0.0,"i")') a%diag(1), a%upper(1)
-        do i=2,n-1
+        do i=2, n - 1
             write(*,'(F0.0,SP,F0.0,"i", F0.0,SP,F0.0,"i", F0.0,SP,F0.0,"i")') a%lower(i-1), a%diag(i), a%upper(i)
         end do
         write(*,'(F0.0,SP,F0.0,"i", F0.0,SP,F0.0,"i")') a%lower(n-1), a%diag(n)
@@ -48,12 +50,40 @@ contains
             
         allocate(comp%diag(size(a%diag)), comp%upper(size(a%upper)), comp%lower(size(a%lower)))
         
-        do i = 1, size(a%diag) - 1
+        do i = 1, size(a%upper)
             comp%lower(i) = a%lower(i) * c
             comp%upper(i) = a%upper(i) * c
             comp%diag(i) = a%diag(i) * c
         end do
-        comp%diag(i + 1) = a%diag(i + 1) * c
+        comp%diag(size(a%diag)) = a%diag(size(a%diag)) * c
         return
     end function
+    
+    !*************************************
+    !           matrix addition          *
+    !*************************************
+    !    returns the value of a tridiagonal
+    !           times a scalar           *          
+    !*************************************
+    function m_add(a, b) result(comp)
+        implicit none
+        !argument variables
+        type(trid)      :: a, b
+        !local variables
+        integer        :: i
+        !return variables
+        type(trid)      :: comp
+            
+        allocate(comp%diag(size(a%diag)), comp%upper(size(a%upper)), comp%lower(size(a%lower)))
+        
+        do i = 1, size(a%lower)
+            comp%lower(i) = a%lower(i) + b%lower(i)
+            comp%upper(i) = a%upper(i) + b%upper(i)
+            comp%diag(i) = a%diag(i) + b%diag(i)
+        end do
+        comp%diag(size(a%diag)) = a%diag(size(a%diag)) + b%diag(size(a%diag))
+        
+        return
+    end function
+    
 end module tridiag
--- tridiag_driver.f90
+++ tridiag_driver.f90
@@ -3,23 +3,32 @@ program tridiag_driver
     !loop counter
     integer :: i 
     !tridiagonal matrix polynomial
-    type(trid) :: a
+    type(trid) :: a, b
 	
 	!allocating arrays
-    allocate(a%diag(10), a%upper(9), a%lower(9))
+    allocate(a%diag(5), a%upper(4), a%lower(4))
 	
 	!putting numbers in the arrays
-    do i = 1, 9
+    do i = 1, 4
         a%diag(i) = i
         a%upper(i) = i
         a%lower(i) = i
     end do
         
-    a%diag(10) = 10
-    call print_trid(a, 10)
+    a%diag(5) = 5
+    call print_trid(a)
+    
+    write(*,'(A)') 'That was A.'
     
     a = s_mult(a, 5) 
-    call print_trid(a, 10)
+    call print_trid(a)
+    
+    write(*,'(A)') 'That was A * 5.'
+
+    b = m_add(a, a)
+    call print_trid(b)
+    
+    write(*,'(A)') 'That was 10A.'
     
     deallocate(a%diag, a%upper, a%lower)
     
